45089a613cd0e1e6194dc1e042167600b4569a26
Briggoh <H.e.briggs99@gmail.com>
2022-11-28 16:17:51 -0800

    minor changes

--- models/Product.js
+++ models/Product.js
@@ -17,7 +17,7 @@ Product.init(
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNUll: false
+      allowNull: false
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
